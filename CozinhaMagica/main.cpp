95f900118ea87c88e6ef767fd8387e2bb32c4dfa
Mr.Viana <rafaelgov95@gmail.com>
2017-04-02 00:37:40 -0400

    Update

--- CozinhaMagica/main.cpp
+++ CozinhaMagica/main.cpp
@@ -7,37 +7,39 @@
 int main() {
 
     Cardapio cardapio;
-    int c, i, t;
+    int c, i, t, qt_cozinheiro, qt_mesa;
     string p;
     cin >> c;
     for (int j = 0; j < c; j++) {
         cin >> i >> t;
         getline(cin, p);
         cardapio.addComida(i, t, p);
-
     }
-    Restaurante rest(3, 10, cardapio);
+    cin >> qt_cozinheiro >> qt_mesa;
+    Restaurante rest(qt_cozinheiro, qt_mesa, cardapio);
     bool flag = true;
     string pedido;
     int mesa;
     regex integer("(\\+|-)?[[:digit:]]+");
     while (flag) {
-        cin >> pedido >> mesa;
+        cin >> pedido;
         if (regex_match(pedido, integer)) {
-            Comida *comi = rest.BuscarComida((unsigned int)atoi(pedido.c_str()));
+            cin >> mesa;
+            cout << pedido << endl;
+            Comida *comi = rest.BuscarComida((unsigned int) atoi(pedido.c_str()));
             if (comi != nullptr) {
                 Pedido p(*comi, mesa);
                 rest.EnviarPedido(p);
             } else {
-                cout << "Essa opção não esta em nosso cardapio" << endl;
+                cout << "ESSA OPÇÃO NÃO ESTA EM NOSSO CARDÁPIO" << endl;
             }
         } else {
             if (pedido.compare("FIM") == 0) {
-                cout << "O espediente foi encerado por hoje !!. Obrigado Pela Preferência ! " << endl;
+                cout << "O EXPEDIENTE FOI ENCERRADO POR HOJE !!. OBRIGADO PELA PREFERÊNCIA ! " << endl;
                 delete &rest;
                 flag = false;
             } else {
-                cout << "ENTRADA INVALIDA" << endl;
+                    cout << "ENTRADA INVÁLIDA" << endl;
             }
         }
 
